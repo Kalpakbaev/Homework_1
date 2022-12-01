5d637df883489614b5d5d852555a330e808bf0e7
Kalpakbaev <kozha_1992@mail.ru>
2022-12-01 06:59:36 +0500

    Задача №3

---
+++ HomeWork_1_3/Program.cs
@@ -0,0 +1,18 @@
+﻿int a = 4;
+if(a % 2 == 0){
+    Console.WriteLine("Число " + a + " Четное ");
+}else{
+    Console.WriteLine("Число  " + a + " Нечетное");
+}
+int b = -3;
+if(b % 2 == 0){
+    Console.WriteLine("Число " + b + " Четное ");
+}else{
+    Console.WriteLine("Число  " + b + " Нечетное");
+}
+int c = 7;
+if (c % 2 == 0){
+    Console.WriteLine("Число " + c + " Четное ");
+}else{
+    Console.WriteLine("Число  " + c + " Нечетное");
+}
