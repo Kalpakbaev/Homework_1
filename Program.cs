e31aa43e6d994a653d77e139115bb07dae3c8212
Kalpakbaev <kozha_1992@mail.ru>
2022-12-01 05:54:02 +0500

    Решили задачу №1

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Hello");
+int a = 5;
+int b = 7;
+int c = 2;
+int d = 10;
+int e = -9;
+int f = -3;
+Console.WriteLine("Число а = " + a);
+Console.WriteLine("Число b = " + b);
+Console.WriteLine("Число с = " + c);
+Console.WriteLine("Число d = " + d);
+Console.WriteLine("Число е = " + e);
+Console.WriteLine("Число f = " + f);
+Console.WriteLine();
+if (a > b)
+Console.WriteLine("Число а больше чем b " + a);  
+else
+{
+    Console.WriteLine("Число b больше чем a " + b);
+}
+if (c > d)
+{
+    Console.WriteLine("Число c больше d " + c);
+}
+else
+{
+    Console.WriteLine("Число d больше чем c " + d);
+}
+if (e > f)
+{
+    Console.WriteLine("Число e больше f " + e);
+}
+else
+{
+    Console.WriteLine("Число f больше чем e " + f);
+}
